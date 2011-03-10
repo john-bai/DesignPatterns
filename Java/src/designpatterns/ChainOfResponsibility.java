023665817c09bb7c2d90430745d770ec1752a081
John Therrell <jtherrell@gmail.com>
2011-03-09 21:32:50 -0800

    Added Chain of Responsibility pattern and tests.

--- Java/src/designpatterns/ChainOfResponsibility.java
+++ Java/src/designpatterns/ChainOfResponsibility.java
@@ -9,6 +9,82 @@ package designpatterns;
  *
  * @author jtherrell
  */
-class ChainOfResponsibility {
+abstract class HelpHandler {
+	protected static final int NO_HELP_TOPIC = -1;
+	public static Log log;
+	
+	protected HelpHandler successor;
+	protected int helpTopicID;
 
+	public HelpHandler(HelpHandler successor, int helpTopicID) {
+		this.successor = successor;
+		this.helpTopicID = helpTopicID;
+	}
+
+	public boolean hasHelpFor(int helpTopicID) {
+		return this.helpTopicID == helpTopicID;
+	}
+
+    public void handleHelp(int helpTopicID) {
+		log.append("In HelpHandler...");
+		if (successor != null)
+			successor.handleHelp(helpTopicID);
+	}
+}
+
+class HelpWidget extends HelpHandler {
+	private HelpWidget parent;
+
+	public HelpWidget(HelpWidget widget, int helpTopicID) {
+		super(widget, helpTopicID);
+		parent = widget;
+	}
+}
+
+class HelpButton extends HelpWidget {
+	public HelpButton(HelpWidget widget, int helpTopicID) {
+		super(widget, helpTopicID);
+	}
+
+	@Override
+	public void handleHelp(int helpTopicID) {
+		log.append("In HelpButton...");
+		if (hasHelpFor(helpTopicID)) {
+			log.append("Button helped!");
+		} else {
+			super.handleHelp(helpTopicID);
+		}
+	}
+}
+
+class HelpDialog extends HelpWidget {
+	public HelpDialog(HelpWidget widget, int helpTopicID) {
+		super(widget, helpTopicID);
+	}
+
+	@Override
+	public void handleHelp(int helpTopicID) {
+		log.append("In HelpDialog...");
+		if (hasHelpFor(helpTopicID)) {
+			log.append("Dialog helped!");
+		} else {
+			super.handleHelp(helpTopicID);
+		}
+	}
+}
+
+class HelpApplication extends HelpWidget {
+	public HelpApplication(int helpTopicID) {
+		super(null, helpTopicID);
+	}
+
+	@Override
+	public void handleHelp(int helpTopicID) {
+		log.append("In HelpApplication...");
+		if (hasHelpFor(helpTopicID)) {
+			log.append("Application helped!");
+		} else {
+			super.handleHelp(helpTopicID);
+		}
+	}
 }
---
+++ Java/test/designpatterns/ChainOfResponsibilityTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package designpatterns;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jtherrell
+ */
+public class ChainOfResponsibilityTest {
+	final int PRINT_TOPIC = 1;
+	final int PAPER_ORIENTATION_TOPIC = 2;
+	final int APPLICATION_TOPIC = 3;
+
+	private HelpApplication application;
+	private HelpDialog dialog;
+	private HelpButton button;
+
+	private Log log;
+
+    public ChainOfResponsibilityTest() {
+		log = new Log();
+		HelpHandler.log = log;
+		application = new HelpApplication(APPLICATION_TOPIC);
+		dialog = new HelpDialog(application, PRINT_TOPIC);
+		button = new HelpButton(dialog, PAPER_ORIENTATION_TOPIC);
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void testExpectedButtonHandle() {
+		button.handleHelp(PAPER_ORIENTATION_TOPIC);
+		String expResult = "In HelpButton...Button helped!";
+		String result = log.toString();
+		assertEquals(expResult, result);
+	}
+
+	@Test
+    public void testExpectedDialogHandle() {
+		button.handleHelp(PRINT_TOPIC);
+		String expResult = "In HelpButton...In HelpHandler..." +
+						   "In HelpDialog...Dialog helped!";
+		String result = log.toString();
+		assertEquals(expResult, result);
+	}
+
+	@Test
+    public void testExpectedApplicationHandle() {
+		button.handleHelp(APPLICATION_TOPIC);
+		String expResult = "In HelpButton...In HelpHandler..." +
+						   "In HelpDialog...In HelpHandler..." +
+						   "In HelpApplication...Application helped!";
+		String result = log.toString();
+		assertEquals(expResult, result);
+	}
+}
